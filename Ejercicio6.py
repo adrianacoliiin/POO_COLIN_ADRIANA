e59e68cc0fbf9642f91eb187d60916ea46cba520
Adriana Colin <adriana_3127230713@utd.edu.mx>
2024-05-20 12:07:09 -0600

    Programa 6 - Número mayor

---
+++ Ejercicio6.py
@@ -0,0 +1,9 @@
+n1 = float(input("Ingresa el primer número: "))
+n2 = float(input("Ingresa el segundo número: "))
+
+if n1 == n2:
+    print("Los números que ingresaste son iguales")
+elif n1 > n2:
+    print("El primer número es mayor")
+else:
+    print("El segundo número es mayor")
