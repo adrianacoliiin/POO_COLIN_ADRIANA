c02780cb81aedcce127da359a4966e5a21118f64
Adriana Colin <adriana_3127230713@utd.edu.mx>
2024-05-20 11:50:43 -0600

    primeros ejercicios

---
+++ Ejercicio2.py
@@ -0,0 +1,3 @@
+nombre = (input("Cual es tu nombre?"))
+print("Hola", nombre)
+ 
---
+++ Ejercicio3.py
@@ -0,0 +1,4 @@
+peso = float(input("Ingrese su peso en kg: "))
+estatura = float(input("Ingrese su altura en m: "))
+imc = peso / estatura ** 2
+print("Tu indice de masa corporal es: ", imc)
