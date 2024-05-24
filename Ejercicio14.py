ddb8c86ee5fb0578f221d66d4cd1130222aef8e6
Adriana Colin <adriana_3127230713@utd.edu.mx>
2024-05-24 07:30:57 -0600

    Programa 14 - for operación

---
+++ Ejercicio14.py
@@ -0,0 +1,8 @@
+numeros = [1,2,3,4,5,6,7,8,9]
+
+suma = 0
+for n in numeros:
+    suma += n
+    
+print("Los valores de la lista son: ", numeros)
+print("La suma de los valores dentro de la lista es de: ", suma)
