84536acc97221acab67d93d72cb5a1b90d917d30
Adriana Colin <adriana_3127230713@utd.edu.mx>
2024-06-08 16:55:00 -0600

    Programa 21 - Tupla desempaquetada

---
+++ Ejercicio21.py
@@ -0,0 +1,10 @@
+persona = ("Miguel", 19, "Ing. Mecatrónica", "2do", "San Luis Potosí")
+
+nombre, edad, carrera, semestre, ubicación = persona
+
+
+print(nombre)
+print(edad)
+print(carrera)
+print(semestre)
+print(ubicación)
