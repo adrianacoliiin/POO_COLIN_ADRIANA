04bb07d8221bc5fb063afc639ab21ea860c1c5e0
Adriana Colin <adriana_3127230713@utd.edu.mx>
2024-06-20 07:51:47 -0600

    Programa 23 - Clases y Objetos

--- Ejercicio23.py
+++ Ejercicio23.py
@@ -1,9 +1,10 @@
-class Estudiante():
+class Estudiante():  #Aqui se crea la clase
     def __init__(self, nombre, edad, semestre):
         self.nombre = nombre
         self.edad = edad
         self.semestre = semestre
     
+    #Definicion de métodos de la clase
     def estudiar(self):
         print("Estudiando...")
 
@@ -12,18 +13,22 @@ class Estudiante():
 
     def hablar(self):
         print("bla, bla, bla...")
-
+ 
+#Creación de objetos de clase estudiante
 estudiante_1 = Estudiante("Miguel", 19, "2do")
 estudiante_2 = Estudiante("Adriana", 18, "3ro")
 
+#Prueba de atributos del objeto 1
 print(estudiante_1.nombre)
 print(estudiante_1.edad)
 print(estudiante_1.semestre)
 
+#Prueba de atributos del objeto 2
 print(estudiante_2.nombre)
 print(estudiante_2.edad)
 print(estudiante_2.semestre)
 
+#Pruebas de los métodos
 estudiante_1.estudiar()
 estudiante_1.participar()
 
